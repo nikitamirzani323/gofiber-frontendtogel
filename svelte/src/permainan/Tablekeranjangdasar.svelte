333b39c6c618fa7b3b38459832df12b505e29545
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-03-06 03:44:42 +0700

    Update Form Dasar Kei - Mobile

--- svelte/src/permainan/Tablekeranjangdasar.svelte
+++ svelte/src/permainan/Tablekeranjangdasar.svelte
@@ -151,6 +151,7 @@
                         <th width="20%" style="text-align:center;vertical-align:top;background:#303030;font-size:11px;border-bottom:none;" NOWRAP>PERMAINAN</th>
                         <th width="20%" style="text-align:right;vertical-align:top;background:#303030;font-size:11px;border-bottom:none;" NOWRAP>BET</th>
                         <th width="20%" style="text-align:right;vertical-align:top;background:#303030;font-size:11px;border-bottom:none;" NOWRAP>DISKON</th>
+                        <th width="20%" style="text-align:right;vertical-align:top;background:#303030;font-size:11px;border-bottom:none;"  NOWRAP>KEI</th>
                         <th width="*" style="text-align:right;vertical-align:top;background:#303030;font-size:11px;border-bottom:none;" NOWRAP>BAYAR</th>
                     </tr>
                 </thead>
@@ -174,6 +175,12 @@
                                 )}
                                 <br />
                                 ({(rec.diskonpercen * 100).toFixed(2)}%)
+                            </td>
+                            <td style="text-align:right;vertical-align:top;font-size:12px;color:#fc0;">
+                                {new Intl.NumberFormat().format(
+                                    Math.ceil(rec.kei)
+                                )}
+                                ({(rec.kei_percen * 100).toFixed(2)}%)
                             </td>
                             <td  style="text-align:right;vertical-align:top;font-size:11px;color:#fc0;">
                                 {new Intl.NumberFormat().format(rec.bayar)}
